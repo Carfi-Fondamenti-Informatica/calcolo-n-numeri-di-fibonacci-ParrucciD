389e2758430260a2b29e2c69a073bc1fcb608ffd
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-11-10 15:02:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-   return 0;
+int main () {
+    int i, n, primo=1, secondo=1, terzo;
+
+
+        cin>>n;
+    if(n<2){
+    cout<< "errore" << endl;
+}else{
+
+    cout<<primo<< endl;
+    cout <<secondo << endl;
+
+    for(i=2;i<n;i++){
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+        cout<<terzo << endl;
+    }
+}
+
+    return 0;
 }
